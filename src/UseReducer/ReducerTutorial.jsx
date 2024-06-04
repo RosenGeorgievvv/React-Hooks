3f213b277f9caea45700a621d66c7564254b70b2
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-04 17:21:12 +0300

    useReducer hook described

--- src/UseReducer/ReducerTutorial.jsx
+++ src/UseReducer/ReducerTutorial.jsx
@@ -2,7 +2,7 @@ import { useReducer } from "react";
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "INCREMENET":
+    case "INCREMENT":
       return { count: state.count + 1, showText: state.showText };
     case "toggleShowText":
         return {count: state.count, showText: !state.showText};
@@ -16,7 +16,7 @@ const ReducerTutorial = () => {
   return (
     <div>
       <h1>{state.count}</h1>
-      <button onClick={() => {dispatch({type: "INCREMENT"})}}>Click Here</button>
+      <button onClick={() => {dispatch({type: "INCREMENT"}); dispatch({type: 'toggleShowText'})}}>Click Here</button>
       {state.showText && <p>Show my text</p>}
     </div>
   );
