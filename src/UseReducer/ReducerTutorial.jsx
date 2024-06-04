685d7700da274b2ae3ffddd0f365d40ef65d4fb1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-04 16:22:57 +0300

    Update ReducerTutorial.jsx

--- src/UseReducer/ReducerTutorial.jsx
+++ src/UseReducer/ReducerTutorial.jsx
@@ -1,12 +1,14 @@
 import { useReducer } from "react";
 
 const ReducerTutorial = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
 
-    const [state, dispatch] = useReducer(reducer, {count: 0, showText: true});
-
-    return(
-
-    )
-}
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => {}}>Click Here</button>
+    </div>
+  );
+};
 
 export default ReducerTutorial;
