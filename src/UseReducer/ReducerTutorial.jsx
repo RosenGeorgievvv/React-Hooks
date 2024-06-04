6e3aad29d2801e118b720d30acae4b4cf6aaf071
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-04 16:26:33 +0300

    Update ReducerTutorial.jsx

--- src/UseReducer/ReducerTutorial.jsx
+++ src/UseReducer/ReducerTutorial.jsx
@@ -1,9 +1,14 @@
 import { useReducer } from "react";
 
 const reducer = (state, action) => {
-    switch(action.type){
-        case "INCREMENET": return {count: state.count + 1, showText: state.showText}
-    }
+  switch (action.type) {
+    case "INCREMENET":
+      return { count: state.count + 1, showText: state.showText };
+    case "toggleShowText":
+        return {count: state.count, showText: !state.showText};
+        default:
+            return state;
+  }
 };
 const ReducerTutorial = () => {
   const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
