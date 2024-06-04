5507b9a697d675f7d3c7670050713bc97e5f3a7b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-04 16:20:46 +0300

    Update ReducerTutorial.jsx

--- src/UseReducer/ReducerTutorial.jsx
+++ src/UseReducer/ReducerTutorial.jsx
@@ -2,7 +2,7 @@ import { useReducer } from "react";
 
 const ReducerTutorial = () => {
 
-    const [state, dispatch] = useReducer()
+    const [state, dispatch] = useReducer(reducer, {});
 
     return(
 
