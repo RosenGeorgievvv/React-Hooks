0a2843f16e9a0cb99f0e92fc3520ac80a62047c2
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-05 16:50:45 +0300

    Update ContextTutorial.jsx

--- src/UseContext/ContextTutorial.jsx
+++ src/UseContext/ContextTutorial.jsx
@@ -15,3 +15,13 @@ function ContextTutorial(){
     )
 }
 export default ContextTutorial;
+
+
+//technical explanation:
+//UseContext hook is used for managing a lot of states.
+//STEPS to configure:
+
+//1.)Import createContext
+//2.)call createContext and store it in a variable (e.g) const AppContext = createContext();
+//3.)Wrap the components inside <AppContext.Provider> ... < /AppContext.Provider>;
+//4.)In value you should pass states and functions you want to have access inside the provider.
