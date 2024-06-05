81abf57a104ae7d38b7c65c4edb5fa16ec7b83b8
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-05 09:08:36 +0300

    Update RefTutorial.jsx

--- src/UseRef/RefTutorial.jsx
+++ src/UseRef/RefTutorial.jsx
@@ -1,1 +1,12 @@
 import { useRef } from "react";
+ 
+const RefTutorial = () =>{
+
+    return(
+        <div>
+            <h1>Rosen</h1>
+            <input type="text" placeholder="enter a name" />
+            <button>Change Name</button>
+        </div>
+    )
+}
