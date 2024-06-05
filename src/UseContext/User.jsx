ebfdf8fe0c1406103e238cbaeb278d420b147c0c
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-05 16:52:38 +0300

    Update User.jsx

--- src/UseContext/User.jsx
+++ src/UseContext/User.jsx
@@ -1,9 +1,8 @@
 import { useContext } from "react";
 import { AppContext } from "./ContextTutorial";
 
-const {username} = useContext(AppContext);
-
-function User(props){
+function User(){
+    const {username} = useContext(AppContext);
     return(
         <div>
             <h1>User: {username}</h1>
